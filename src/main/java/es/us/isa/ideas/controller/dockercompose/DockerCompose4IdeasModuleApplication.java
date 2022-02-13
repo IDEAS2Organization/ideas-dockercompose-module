9829a5747a2e7f38e74f918f49178ad67ef5fc9e
Francisco-Borrego <fraborcar@alum.us.es>
2022-02-13 16:56:05 +0100

    Commit inicial

---
+++ src/main/java/es/us/isa/ideas/controller/dockercompose/DockerCompose4IdeasModuleApplication.java
@@ -0,0 +1,11 @@
+package es.us.isa.ideas.controller.dockercompose;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DockerCompose4IdeasModuleApplication {
+        public static void main(String[] args){
+            SpringApplication.run(DockerCompose4IdeasModuleApplication.class,args);
+        }
+}
